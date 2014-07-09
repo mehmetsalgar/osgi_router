aaab90a8cd825f02579bc7d1048828ffedd59752
Mehmet Salgar <salgarm@gmx.de>
2014-07-09 14:25:04 +0200

    Initial commit

---
+++ osgi-b2b-mediation/src/test/java/org/salgar/xpath/XPathTest.java
@@ -0,0 +1,13 @@
+package org.salgar.xpath;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+public class XPathTest {
+	public void v1Test() {
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		
+		NamespaceContext namespaceContext = null;
+	}
+}
